b9fc3a33a4c0a4509d557d0e4694bb57646afa31
JIAWEI CHEN <jiaweichen@JIAWEIs-MacBook-Pro.local>
2017-03-01 15:32:02 -0800

    Initial Commit

---
+++ PokedexGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokedexGo
+//
+//  Created by JIAWEI CHEN on 3/1/17.
+//  Copyright © 2017 PokGear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
